5ea51db5156dfb7a87bd4851353483ba80e21482
sage_koala <brandondukescodes@gmail.com>
2025-02-19 17:41:54 -0600

    Add background.js service worker to listen and responsd to activeTab

---
+++ chrome_extension/background.js
@@ -0,0 +1,30 @@
+// Monitors activeTab, extracts videoId from YouTube videos, sends ID to backend 
+// to extract video info from YouTube API to include title, audio language, duration
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    if (changeInfo['url'] && changeInfo['url'].includes("youtube.com/watch")) {
+        const videoId = (changeInfo['url'].split("v="))[1];
+        console.log(videoId);    
+        
+        // Fetch YouTube API data from Flask server
+        const sendDataToBackend = async (data) => {
+            try {
+                const response = await fetch("http://127.0.0.1:5000/", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify(data),
+                });
+        
+                const result = await response.json();
+                console.log("Response from server:", result);
+            } catch (error) {
+                console.error("Error sending data:", error);
+            }
+        };
+        
+        // Example usage
+        sendDataToBackend({ videoId: videoId });
+    }
+})
+
