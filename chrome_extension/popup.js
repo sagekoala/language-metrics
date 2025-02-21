6a4ac7c2876fd1b66e8537ad2d32bdb0538586c6
sage_koala <brandondukescodes@gmail.com>
2025-02-20 22:56:26 -0600

    Add event listener to "View Watch History" anchor tag and create new tab with /history route on server as destination

--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -7,8 +7,9 @@
             // Display metrics in the popup
             document.getElementById('metrics').innerHTML = `
                 <p><strong>Target Language:</strong> ${data.target_language}</p>
-                <p><strong>Time Spent Listening in the Last 7 Days:</strong> ${data.total_minutes_7_days} minutes</p>
-                <p><strong>Time Spent Listening All Time:</strong> ${data.total_minutes_all_time} minutes</p>
+                <p><strong>Time Spent Listening Yesterday:</strong> ${data.yesterday_minutes} minutes</p>
+                 <p><strong>Time Spent Listening Today:</strong> ${data.today_minutes} minutes</p>
+                <p><strong>Time Spent Listening All Time:</strong> ${Math.floor(data.total_minutes_all_time/60)} hours ${data.total_minutes_all_time%60} minutes</p>
             `;
         } catch (error) {
             console.error("Error fetching metrics:", error);
@@ -18,3 +19,11 @@
 
     // Fetch and display the metrics when the popup is loaded
     fetchMetrics();
+
+    // Add event listener to anchor tag
+    document.getElementById('view-history').addEventListener('click', function(event) {
+        event.preventDefault();  // Prevent default anchor behavior
+        
+        // Open the watch history page in a new tab
+        chrome.tabs.create({ url: "http://127.0.0.1:5000/history" });
+    });
